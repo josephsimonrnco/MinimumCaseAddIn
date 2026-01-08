21eebcea9efe8978966f3c459f365c3a8fbc60be
josephsimonrnco <joseph.simon@rothschildandco.com>
2026-01-08 12:01:39 +0000

    implement the changes suggested by govert in https://groups.google.com/g/exceldna/c/kRiUhAjD7t8
    add dummy addin in FunctionsExternal to prevent error message of 'No Objects Loaded'

--- FunctionsExternal/SuperAddition.cs
+++ FunctionsExternal/SuperAddition.cs
@@ -1,6 +1,11 @@
 ﻿using ExcelDna.Integration;
 
 namespace FunctionsExternal {
+    public class DummyAddIn : IExcelAddIn {
+        public void AutoOpen() { }
+
+        public void AutoClose() {}
+    }
     public class SuperAddition {
         [ExcelFunction(Name = "SUPAADD", Description = "Super Addition, it happens, but you cant control the inputs")]
         public static object[,] SupaAdd() {
